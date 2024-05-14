db0c369b8a3762d95d447f107d852d502d1bef5f
TothMessiah <163463550+TothMessiah@users.noreply.github.com>
2024-05-14 21:30:18 +0000

    U3l1

--- fairyTale.js
+++ fairyTale.js
@@ -1,28 +1,49 @@
-// 1. Create a variable named `place` and set the value to a string that says whatever place you want.
+// 1. Create a variable named `place` and set the value to a string that says whatever place you want
+place = 'MilkFactory'
 
 // 2. Create a variable named `creatureName` and set the value to a string that says whatever name you want to give the creature.
 
+creatureName = 'MilkMan'
+
 // 3. Create a variable named `color` and set the value to a string that says whatever color you want.
+
+favoriteColor = 'Cyan';
 
 // 4. Create a variable named `color2` and set the value to a string that says a different color you want.
 
+favoriteColor2 = 'Green';
+
 // 5. Create a variable named `emotion` and set the value to a string that says whatever emotion you want.
+
+emotion = 'Sad';
 
 // 6. Create a variable named `character` and set the value to a string that says whatever name for a character you want.
 
+character = 'Cyphor';
+
 // 7. Create a variable named `number` and set the value to a number greater than 1.
+
+favoriteNumber = 8;
 
 // 8. Create a variable named `item` and set the value to a string that says whatever item/thing you want.
 
+item = 'Milk';
+
 // 9. Create a variable named `item2` and set the value to a string that says a different item/thing you want.
+
+item2 = 'Cheese';
 
 // 10. Create a variable named `verb` and set the value to a string that says whatever verb you want.
 
+verb = 'Milked';
+
 // 11. Create a variable named `adverb` and set the value to a string that says whatever adverb you want.
+
+adverb = 'Milkily';
 
 // 12. Create a variable named `number2` and set the value to a number greater than 1.
 
-
+favoriteNumber2 = '9';
 
 
 
